40b3a83f1e7279e93eb597be034539b6d3f71875
BenW <bmw95@pitt.edu>
2018-11-27 15:03:07 -0500

    Added updateStats()

--- ga.py
+++ ga.py
@@ -17,14 +17,16 @@ class chrom:
         return chrom(self.LR,self.maxE,self.L1,self.L2,self.L3,self.fitness)
     def calculateFitness(self):
         error,epochs = tensor.nn(self.LR,self.maxE,self.L1,self.L2,self.L3)
-        #print(error,epochs)
         #Fitness is found by taking error as a multiplier against a flat bonus and subtracting out time taken
         self.fitness = float((100*error)-epochs)
     def print(self,num):
+        #Prints out all chrome data
         print("#"+str(num)+ "|Fitness="+str(self.fitness) + "|LR="+ str(self.LR) + "|Max Epoch ="+str(self.maxE) + "|L1=" + str(self.L1) + "|L2="+ str(self.L2)+"|L3="+str(self.L3))
 class Population:
     def __init__(self):
+        #List of chromes
         self.pop = []
+        #Statisics
         self.min = 10000000
         self.max = -10000000
         self.avg = 0
@@ -34,7 +36,7 @@ class Population:
         counter = 0
         #Max PopSize chroms of randomized values, add to list
         for x in range(PopSize):
-            Population.append(chrom(random.uniform(.001,.01),random.randint(1,5000),random.randint(800,5000),random.randint(800,5000),random.randint(800,5000)))
+            Population.append(chrom(random.uniform(.001,.05),random.randint(1,2000),random.randint(800,5000),random.randint(800,5000),random.randint(800,5000)))
         #Calculated the fitness of each, record statistics as fitness is updated
         for p in Population:
             print("==Getting Fitness for chrom:" + str(counter)+ "======")
@@ -47,8 +49,26 @@ class Population:
             elif (p.fitness>self.max):
                 self.max = p.fitness
         self.avg = total/PopSize
-
         self.pop = Population
+    def updateStats(self):
+        #Reset Stats
+        self.min = 10000000
+        self.max = -10000000
+        self.avg = 0
+        total = 0
+        counter = 0
+        #Get fitness for each chrom, update statistics if needed
+        for p in self.pop:
+            print("==Getting Fitness for chrom:" + str(counter)+ "======")
+            p.calculateFitness()
+            print("Fitness for "+str(counter) + "=" + str(p.fitness))
+            counter += 1
+            total += p.fitness
+            if (p.fitness<self.min):
+                self.min = p.fitness
+            elif (p.fitness>self.max):
+                self.max = p.fitness
+        self.avg = total/len(self.pop)
     def print(self,gen = 0):
         numCounter = 0
         print("=========Generation "+ str(gen) +"==============")
@@ -77,12 +97,7 @@ else:
     #quit
     print("Run as: python ga.py N cR mR")
     quit()
-#=================================
+#============Init Population=================
 P = Population()
 P.initPopulation(PopSize)
-P.print()
 
-#test = chrom(.003,500,2432,2322,1943)
-#test.calculateFitness()
-#test.print()
-#print(tensor.nn())
